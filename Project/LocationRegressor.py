e3fa45d501ec907ee26bd3587afed8f6f5c461ce
Kevin Paul <kevinklaiber9782@gmail.com>
2025-02-01 16:07:54 +0100

    Overwhelmed rn

---
+++ Project/LocationRegressor.py
@@ -0,0 +1,32 @@
+# A regression model to predict the location of a given image with two output nodes
+# The CNN model sees the image in black and white and predicts the x and y coordinates (controid of the earth)
+# It has two output nodes, one for x and one for y
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class LocationRegressor(nn.Module):
+    def __init__(self):
+        super(LocationRegressor, self).__init__()
+
+        # Using nn.Sequential to define the model
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.MaxPool2d(4),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.MaxPool2d(4),
+            nn.ReLU(),e
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.MaxPool2d(4),
+            nn.ReLU(), # Next the linear layers for regression
+            nn.Flatten(),
+            nn.Linear(256, 2)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
